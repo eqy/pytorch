6eedb88b23ef42d1207f15301b1c8d065e7c7ae6
Eddie Yan <eddiey@nvidia.com>
2025-02-07 23:36:59 +0000

    add dtype fuzz

--- fuzz.py
+++ fuzz.py
@@ -24,13 +24,16 @@ MIN_DMOD = 8
 MIN_DV = 1
 MAX_DV = 128
 MAX_ELEM = 2**29
-CHECK_FP32REF = True
+CHECK_REF = True
+REF_DTYPE = torch.half
 
 i = 0
 num_gpus = torch.cuda.device_count()
+dtypes = [torch.half, torch.bfloat16]
 
 while True:
     device = i % num_gpus
+    dtype = dtypes[torch.randint(low=0, high=len(dtypes), size=(1,)).item()]
     b = torch.randint(low=MIN_B, high=MAX_B+1, size=(1,)).item()
     high_sq = int(min(MAX_SEQLEN_Q, MAX_ELEM/b) + 1)
     high_skv = int(min(MAX_SEQLEN_KV, MAX_ELEM/b) + 1)
@@ -79,53 +82,54 @@ while True:
     qfillshape = [[b, h_q, s_q, d_qk][idx] for idx in q_permute]
     kfillshape = [[b, h_k, s_kv, d_qk][idx] for idx in k_permute]
     vfillshape = [[b, h_v, s_kv, d_v][idx] for idx in v_permute]
-    q = torch.randn(qfillshape, dtype=torch.half, device=f'cuda:{device}', requires_grad=True).permute(q_reverse).detach()
+    q = torch.randn(qfillshape, dtype=dtype, device=f'cuda:{device}', requires_grad=True).permute(q_reverse).detach()
     q.requires_grad=True
-    k = torch.randn(kfillshape, dtype=torch.half, device=f'cuda:{device}', requires_grad=True).permute(k_reverse).detach()
+    k = torch.randn(kfillshape, dtype=dtype, device=f'cuda:{device}', requires_grad=True).permute(k_reverse).detach()
     k.requires_grad=True
-    v = torch.randn(vfillshape, dtype=torch.half, device=f'cuda:{device}', requires_grad=True).permute(v_reverse).detach()
+    v = torch.randn(vfillshape, dtype=dtype, device=f'cuda:{device}', requires_grad=True).permute(v_reverse).detach()
     v.requires_grad=True
     assert q.is_leaf
     assert k.is_leaf
     assert v.is_leaf
 
     grad_outputfillshape = [[b, h_q, s_q, d_v][idx] for idx in grad_permute]
-    grad_output = torch.randn(grad_outputfillshape, dtype=torch.half, device=f'cuda:{device}').permute(grad_reverse)
+    grad_output = torch.randn(grad_outputfillshape, dtype=dtype, device=f'cuda:{device}').permute(grad_reverse)
 
     ref_ok = False
     try:
-        if CHECK_FP32REF:
-          q_ref = q.detach().float()
-          k_ref = k.detach().float()
-          v_ref = v.detach().float()
+        if CHECK_REF:
+          q_ref = q.detach().to(REF_DTYPE)
+          k_ref = k.detach().to(REF_DTYPE)
+          v_ref = v.detach().to(REF_DTYPE)
           q_ref.requires_grad=True
           k_ref.requires_grad=True
           v_ref.requires_grad=True
-          print(q_ref.shape, k_ref.shape, v_ref.shape) 
-          out_ref = F.scaled_dot_product_attention(q_ref, k_ref, v_ref, enable_gqa=True)
-          grad_output_ref = grad_output.float()
-          out_ref.backward(grad_output_ref)
+          print(q_ref.shape, k_ref.shape, v_ref.shape)
+          with sdpa_kernel([SDPBackend.FLASH_ATTENTION, SDPBackend.EFFICIENT_ATTENTION, SDPBackend.MATH]):
+              out_ref = F.scaled_dot_product_attention(q_ref, k_ref, v_ref, enable_gqa=True)
+              grad_output_ref = grad_output.to(REF_DTYPE)
+              out_ref.backward(grad_output_ref)
           ref_ok = True
     except torch.OutOfMemoryError as e:
         print("hit OOM while trying to compute ref...")
         
     try: 
-        if CHECK_FP32REF and ref_ok:
+        if CHECK_REF and ref_ok:
             with sdpa_kernel(SDPBackend.CUDNN_ATTENTION): 
                 assert q.is_leaf
                 assert k.is_leaf
                 assert v.is_leaf
                 out = F.scaled_dot_product_attention(q, k, v)
-                torch.testing.assert_close(out, out_ref.half(), atol=3e-3, rtol=3e-3)
+                torch.testing.assert_close(out, out_ref.dtype(), atol=3e-3, rtol=3e-3)
                 out.backward(grad_output)
                 assert k.grad is not None
                 assert v.grad is not None
                 assert q.grad is not None
                 assert q_ref.grad is not None
 
-                torch.testing.assert_close(q.grad, q_ref.grad.half(), atol=3e-3, rtol=3e-3)
-                torch.testing.assert_close(k.grad, k_ref.grad.half(), atol=3e-3, rtol=3e-3)
-                torch.testing.assert_close(v.grad, v_ref.grad.half(), atol=3e-3, rtol=3e-3)
+                torch.testing.assert_close(q.grad, q_ref.grad.dtype(), atol=3e-3, rtol=3e-3)
+                torch.testing.assert_close(k.grad, k_ref.grad.dtype(), atol=3e-3, rtol=3e-3)
+                torch.testing.assert_close(v.grad, v_ref.grad.dtype(), atol=3e-3, rtol=3e-3)
 
         with sdpa_kernel(SDPBackend.CUDNN_ATTENTION):
             out = F.scaled_dot_product_attention(q, k, v, dropout_p=dropout_p).sum().backward()
