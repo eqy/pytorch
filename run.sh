196809d3966ea4756bb82c1f713c44126b9840aa
Eddie Yan <eddiey@nvidia.com>
2025-02-10 22:51:54 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-torchrun --nproc-per-node $(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l) --nnodes 1
+torchrun --nproc-per-node $(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l) --nnodes 1 fuzz.py
